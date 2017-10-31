b8fbc1e73be0567f942a0caee4b3d5c94d280905
Ann Layman <annelayman@gmail.com>
2017-10-30 21:00:55 -0400

    Fixed indentation

--- 02_linkedlist/linkedlist.js
+++ 02_linkedlist/linkedlist.js
@@ -4,78 +4,44 @@ function LinkedList() {
  }
  
  LinkedList.prototype.addToTail = function(value) {
- 
-        if (this.head === null && this.tail === null) {
-            var nodeInstance = new Node(value);
-            this.head = nodeInstance;
-            this.tail = nodeInstance;
-        }
-        else{
-            var nodeInstance = new Node(value);
-            this.tail.next = nodeInstance;
-            nodeInstance.previous = this.tail;
-            this.tail = nodeInstance;
-            
-        }
-    }; 
+    var nodeInstance = new Node(value);
+    if (this.head === null && this.tail === null) {
+        this.head = nodeInstance;
+        this.tail = nodeInstance;
+    } else{
+        this.tail.next = nodeInstance;
+        nodeInstance.previous = this.tail;
+        this.tail = nodeInstance;
+        
+    }
+}; 
  LinkedList.prototype.removeHead = function() {
-        var prevHead = this.head;
-        this.head = this.head.next;
-        this.head.previous = null;
-        return prevHead.value;
- 
-    };function Queue(){
-   var returnVal = 0
-   var array = [];
-   var counter = 0;
-   this.enqueue = function(str){
-       array[returnVal] = str;
-       // counter++;
-       // array.push(str);
-       returnVal++;      
-   };
+    var prevHead = this.head;
+    this.head = this.head.next;
+    this.head.previous = null;
+    return prevHead.value;
 
-   this.dequeue = function(){
-       if (returnVal === 0) {
-           return undefined;
-       }
-       returnVal--;
-       var captured = array[0];        
-       array = array.slice(1);
-       return captured;
-       // array.pop(str);
-
-   };
-
-   this.size = function(){
-       
-      return returnVal;
-
-   };
-
-}
+};
  
  LinkedList.prototype.search = function() {
  
  };
  
  LinkedList.prototype.addToHead = function(value) {
- 
-        if (this.head === null && this.tail === null) {
-            var nodeInstance = new Node(value);
-            this.head = nodeInstance;
-            this.tail = nodeInstance;
-        }
-        else{
-            var nodeInstance = new Node(value);
-            nodeInstance.next = this.head
-            this.head.previous = nodeInstance;
-            this.head = nodeInstance; 
- 
- 
- 
- 
-        }
+    if (this.head === null && this.tail === null) {
+        var nodeInstance = new Node(value);
+        this.head = nodeInstance;
+        this.tail = nodeInstance;
+    } else{
+        var nodeInstance = new Node(value);
+        nodeInstance.next = this.head
+        this.head.previous = nodeInstance;
+        this.head = nodeInstance; 
+
+
+
+
+    }
  };
  
  
